e67392a71bb4e566448dbf4373e94ebdacf3cbbe
北斗 <1912128546@qq.com>
2021-12-09 22:04:42 +0800

    ✨ feat:完成jsx解析

--- src/MyReact15/react-dom.js
+++ src/MyReact15/react-dom.js
@@ -1,60 +1,99 @@
-//把vnode -> node并插入
-
-// vnode 虚拟节点
-// node 真是节点
+/**
+ *
+ * @param {*} vnode
+ * @param {*} container
+ */
 function render(vnode, container) {
   const node = createNode(vnode)
-
+  console.log('node33', vnode)
+  console.log('node33', node)
   container.appendChild(node)
 }
-function isStringOrNumber(sth) {
-  return typeof sth === 'string' || typeof sth === 'number'
-}
-//将虚拟节点转换为真是节点
+/**
+ *
+ * @param {*} vnode
+ * @returns 真实DOM结构
+ */
 function createNode(vnode) {
-  console.log('vnode', vnode)
   const { type } = vnode
+  console.log('vnode', vnode)
   let node
-
-  //原生标签节点
   if (typeof type === 'string') {
     node = updateHostComponent(vnode)
-  } else if (isStringOrNumber(vnode)) {
-    //文本标签
-    node = updateTextComponent(vnode)
   } else if (typeof type === 'function') {
-    //类组件，函数组件
-    node = type.prototype.isReactComponent
-      ? updateClassComponent(vnode)
-      : updateFunctionComponent(vnode)
-  } else {
-    // fragment标签
-    node = updateFragmentComponent(vnode)
+    node = updateFunctionComponent(vnode)
+  } else if (isStringOrNumber(vnode)) {
+    node = updateTextComponent(vnode)
   }
   return node
 }
-function updateFragmentComponent(vnode) {
-  //const d =
-  const { props } = vnode
-  const node = document.createDocumentFragment()
+
+function updateTextComponent(vnode) {
+  return document.createTextNode(vnode)
+}
+
+/**
+ *判断类型
+ *
+ * @param {*} s
+ * @returns
+ */
+function isStringOrNumber(sth) {
+  return typeof sth === 'string' || typeof sth === 'number'
+}
+
+/**
+ *入参是虚拟dom
+ * @param {*} vnode
+ * 返回真实DOM
+ */
+function updateHostComponent(vnode) {
+  const { type, props } = vnode
+  let node = document.createElement(type)
+  updateNode(node, props)
   reconcileChildren(node, props.children)
   return node
 }
 
-//根据虚拟节点创建类组件
-function updateClassComponent(vnode) {
-  const { type, props } = vnode
-
-  const instance = new type(props)
-  const child = instance.render()
-
-  const node = createNode(child)
-
+/**
+ *
+ *入参是真实节点，给真实节点添加上属性
+ 比如说：calss名，id名，title等
+ * @param {*} node
+ * @param {*} nodeValue
+ * @returns
+ */
+function updateNode(node, nodeValue) {
+  Object.keys(nodeValue)
+    .filter((k) => k !== 'children')
+    .forEach((k) => {
+      node[k] = nodeValue[k]
+    })
   return node
 }
 
-//根据虚拟节点创建函数组件
+/**
+ *调和子节点
+ *
+ * @param {*} parentNode
+ * @param {*} children
+ */
+function reconcileChildren(parentNode, children) {
+  const newChildren = Array.isArray(children) ? children : [children]
+  for (let i = 0; i < newChildren.length; i++) {
+    const child = newChildren[i]
+    console.log('child', child)
+    // vnode 变成node ;并将node插入parentNode
+    render(child || '', parentNode)
+  }
+}
 
+/**
+ *
+ *入参是虚拟dom
+ * @param {*} vnode
+ * @returns 返回真实DOM
+ */
 function updateFunctionComponent(vnode) {
   const { type, props } = vnode
   const child = type(props)
@@ -62,39 +101,4 @@ function updateFunctionComponent(vnode) {
   return node
 }
 
-//根据虚拟节点创建文本节点
-function updateTextComponent(vnode) {
-  const node = document.createTextNode(vnode)
-  return node
-}
-
-//根据虚拟节点生成原生标签节点,diva,p,h1
-function updateHostComponent(vnode) {
-  const { type, props } = vnode
-  const node = document.createElement(type)
-  updateNode(node, props)
-  reconcileChildren(node, props.children)
-  return node
-}
-
-//更新原生标签属性和属性值
-function updateNode(node, nextValue) {
-  Object.keys(nextValue)
-    .filter((k) => k !== 'children')
-    .forEach((k) => {
-      node[k] = nextValue[k]
-    })
-}
-
-// 处理子节点
-function reconcileChildren(parentNode, children) {
-  //将children转换为数组
-  const newChildren = Array.isArray(children) ? children : [children]
-  for (let i = 0; i < newChildren.length; i++) {
-    const child = newChildren[i]
-    // vnode 变成node ;并将node插入parentNode
-
-    render(child || '', parentNode)
-  }
-}
 export default { render }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css'
 
 function App() {
   return (
-    <div>
+    <div className="nanjing">
       <h2>测试</h2>
     </div>
   )
